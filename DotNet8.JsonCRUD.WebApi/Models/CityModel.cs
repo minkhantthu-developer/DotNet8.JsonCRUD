4bf3197f5eaed6c6f94fa0890c64d09c19c2360a
Min Khant Thu <dsalpha3367@gmail.com>
2024-10-29 20:23:05 +0630

    [Add] CityModel

---
+++ DotNet8.JsonCRUD.WebApi/Models/CityModel.cs
@@ -0,0 +1,33 @@
+﻿namespace DotNet8.JsonCRUD.WebApi.Models
+{
+
+    public class CityModel
+    {
+        public List<Datum> data { get; set; }
+    }
+
+    public class Datum
+    {
+        public string eng { get; set; }
+        public string mm { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string capital { get; set; }
+        public List<District> districts { get; set; }
+    }
+
+    public class District
+    {
+        public string eng { get; set; }
+        public string mm { get; set; }
+        public List<Township> townships { get; set; }
+    }
+
+    public class Township
+    {
+        public string eng { get; set; }
+        public string mm { get; set; }
+    }
+
+
+}
